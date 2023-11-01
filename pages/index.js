5e0c062243ac51cd8fd33bd8f336a137342bf4fe
Francis Mozart Andrade <francismozart@gmail.com>
2023-11-01 17:20:05 +0000

    Add a good message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Ashara bassábias</h1>
+    return (
+    <div style={{ width: '100vw' }}>
+        <div style={{ display: 'table', width: 'auto', margin: '0 auto'}}>
+            <h1>te amo, mozão!</h1>
+            <iframe src="https://giphy.com/embed/Cbn1wBJXKsbOKAwA0Q" width="480" height="480" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            <p><a href="https://giphy.com/gifs/TheSwoon-Cbn1wBJXKsbOKAwA0Q"></a></p>
+        </div>
+    </div>
+    )
 }
 
 export default Home
